4665b39ed2b1f9c17cb322c2f6562156fd4ce0df
FlightRipper <101296159+FlightRipper@users.noreply.github.com>
2023-09-25 10:36:44 +0300

    step 3

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require('express')
 const app = express()
 
-app.listen(3000)
+let now = new Date()
+let hours = now.getHours()
+let seconds = now.getSeconds()
 
-app.get("/" , (req , res) => {
+app.get("/" , (req , res) => 
+{
     console.log("Kousa")
     res.send("Kousa")
 }
-
 )
+
+app.get("/test" , (req , res) =>
+{
+    res.status(200).send({ status:res.statusCode, message:"test"})
+}
+)
+app.get("/time" , (req , res) =>
+    res.status(200).send({ status:res.statusCode, message : hours + "" + seconds}
+)
+)
+
+
+
+app.listen(3000)
