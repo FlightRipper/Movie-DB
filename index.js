b909cce6b0ac2f9b2d90cc580cd92188db131259
FlightRipper <101296159+FlightRipper@users.noreply.github.com>
2023-09-27 21:49:57 +0300

    step 4

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
-
+app.listen(3000)
 let now = new Date()
 let hours = now.getHours()
 let seconds = now.getSeconds()
+
 
 app.get("/" , (req , res) => 
 {
@@ -17,11 +18,26 @@ app.get("/test" , (req , res) =>
     res.status(200).send({ status:res.statusCode, message:"test"})
 }
 )
+
 app.get("/time" , (req , res) =>
     res.status(200).send({ status:res.statusCode, message : hours + "" + seconds}
 )
 )
 
+app.get('/hello/:id?', (req, res) => {
+    const {id} = req.params;
+    if (id) {
+        res.status(200).json({ status: 200, message: `Hello, ${id}` });
+    } else {
+      res.status(200).json({ status: 200, message: 'Hello, World!' });
+    }
+})
 
-
-app.listen(3000)
+app.get('/search', (req, res) => {
+    const Kousa = req.query.s;
+    if (Kousa) {
+      res.status(200).json({ status: 200, message: 'ok', data: Kousa });
+    } else {
+      res.status(500).json({ status: 500, error: true, message: 'you have to provide a search' });
+    }
+})
